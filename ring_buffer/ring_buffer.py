c0ef77484c7a058eff866c72dd8cb367951338e5
patrickjwolf <patrickwolf@Lenovo-PC.localdomain>
2020-06-12 10:39:03 -0600

    ring buffer submission

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,22 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
-
+        self.capacity = capacity
+        self.current = 0
+        self.storage = [None]*capacity
+    
     def append(self, item):
-        pass
+        if self.current == self.capacity:
+            self.current = 0
+        self.storage.pop(self.current)
+        self.storage.insert(self.current, item)
+
+        self.current += 1
 
     def get(self):
-        pass
+        elements = list()
+        for item in self.storage:
+            if item is None:
+                continue
+            elements.append(item)
+        
+        return elements
