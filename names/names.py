09688469df2b1d46eb7b0728f38a16dd74d0b4eb
patrickjwolf <patrickwolf@Lenovo-PC.localdomain>
2020-07-17 11:27:55 -0600

    Sprint update

--- names/names.py
+++ names/names.py
@@ -13,21 +13,20 @@ f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
 
-# Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
-
+# # Replace the nested for loops below with your improvements
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 # For each name in names_1, insert name into a binary search tree
-bst_names = BinarySearchTree(names_1[0])
+best_n = BinarySearchTree(names_1[0])
 for name1 in names_1:
-    bst_names.insert(name1)
+    best_n.insert(name1)
 
 # For each name in names_2, check if bst contains name.
 for name2 in names_2:
     # If true, append name from names_2 to duplicates
-    if bst_names.contains(name2):
+    if best_n.contains(name2):
         duplicates.append(name2)
 
 duplicates.sort()
